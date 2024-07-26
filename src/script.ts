aa0abba4a25d33ff084388736c4c39f73cab11cc
xpakx <55586470+xpakx@users.noreply.github.com>
2024-07-26 14:12:30 +0200

    feat: inject html and css

--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,16 @@
-document.body.innerHTML = "<h2>Hello world</h2>";
+console.log("Injecting HTML");
+fetch(browser.runtime.getURL("./pages/index.html"))
+.then(response => response.text())
+.then(html => {
+	document.body.innerHTML = html;
+});
+
+console.log("Injecting CSS");
+fetch(browser.runtime.getURL("./styles/styles.css"))
+.then(response => response.text())
+.then(css => {
+	var style = document.createElement("style");
+	style.innerHTML = css;
+	document.head.insertAdjacentElement('beforeend', style);
+});
+
